4407fe9e40197be3316049815c193c8b7c29371e
MARKO JOVICIC <markoj2203@gmail.com>
2021-02-26 21:37:00 +0100

    format date and time with moment.js

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,6 +1,10 @@
 import React from "react";
+import moment from "moment";
 
 function Messages(data) {
+  const dateTimeFormat = moment(data.data.timestamp).format(
+    "DD MMM YYYY hh:mm"
+  );
   return (
     <div
       className={
@@ -15,9 +19,7 @@ function Messages(data) {
         <div className="card-body">
           <h6 className="card-subtitle mb-2 text-muted">{data.data.author}</h6>
           <h5 className="card-title">{data.data.message}</h5>
-          <h6 className="card-subtitle mb-2 text-muted">
-            {data.data.timestamp}
-          </h6>
+          <h6 className="card-subtitle mb-2 text-muted">{dateTimeFormat}</h6>
         </div>
       </div>
     </div>
