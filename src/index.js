9b885f20599c66401a9515f0b85d0e3fa4895ca4
MARKO JOVICIC <markoj2203@gmail.com>
2021-02-26 11:45:17 +0100

    setting envirement

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
-import App from "./App";
+import App from "./components/App";
+//import { Provider } from "react-redux";
+//import { createStore } from "redux";
+//import rootReducer from "./reducers";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+/*const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);*/
+
+ReactDOM.render(<App />, document.getElementById("root"));
